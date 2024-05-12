8dff355bb017f6748d6e645af4f5950d220d3302
Razkaroth <rocker.ikaros@gmail.com>
2024-05-12 14:48:12 -0600

    discards use of tiptap-editor as an external module

--- apps/smalleditor/src/app/pages/main/main.component.ts
+++ apps/smalleditor/src/app/pages/main/main.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxTiptapNotionComponent } from '@ngx-tiptap-notion'
-import * as Y from 'yjs';
+import { EditorComponent } from './editor/editor.component';
+
 @Component({
   selector: 'smalleditor-main',
   standalone: true,
-  imports: [CommonModule, NgxTiptapNotionComponent],
+  imports: [CommonModule, EditorComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
-  ydoc = new Y.Doc();
 }
